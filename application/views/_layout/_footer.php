399112859dd179b6004b3e3bce39f1221a40eb0d
assyifff <syifanurulfitri@outlook.com>
2023-12-28 05:40:50 +0700

    initial commit website

---
+++ application/views/_layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+		JeWePe
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2023 <a href="#">As Syifa Nurul Fitri Ubaidillah</a>.</strong> All rights reserved.
+</footer>
