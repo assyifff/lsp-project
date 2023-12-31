cb75da68cc28a7421c9da147d47c2d7468df91b7
assyifff <syifanurulfitri@outlook.com>
2023-12-31 11:25:42 +0700

    update login

--- application/views/login.php
+++ application/views/login.php
@@ -1,85 +1,86 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>JeWePe | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/eksternal/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/eksternal/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/iCheck/square/blue.css">
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="<?php echo base_url('assets/img/logo_circle.png'); ?>">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="hold-transition login-page">
+    <!-- Custom Styles -->
+    <style>
+        body {
+            background-color: #f7f7f7;
+        }
+        .login-box {
+            width: 300px;
+            margin: 10% auto;
+        }
+        .login-logo {
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
+            margin-bottom: 20px;
+            color: #333;
+        }
+        .login-box-msg {
+            margin-bottom: 20px;
+            color: #555;
+        }
+        .form-control {
+            border-radius: 20px;
+            border-color: #ccc;
+        }
+        .btn-primary {
+            background-color: #183114; /* Ganti warna sesuai keinginan (#183114) */
+            border-color: #183114;
+            border-radius: 20px;
+            color: #fff;
+        }
+        .btn-primary:hover,
+        .btn-primary:focus {
+            background-color: #0f1f0c; /* Warna saat hover atau focus (#0f1f0c) */
+            border-color: #0f1f0c;
+        }
+    </style>
+</head>
+<body class="hold-transition login-page">
     <div class="login-box">
-      <div class="login-logo">
-        <a href="<?php echo base_url(); ?>assets/index2.html"><b>JeWePe</a>
-      </div>
+        <div class="login-logo">
+            <a href="<?php echo base_url(); ?>assets/index2.html"><b>JeWePe</b></a>
+        </div>
 
-      <!-- /.login-logo -->
-      <div class="login-box-body">
-        <p class="login-box-msg">
-          Log in to start your session
-        </p>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+            <p class="login-box-msg">
+                Log in to start your session
+            </p>
 
-        <form action="<?php echo base_url('Auth/login'); ?>" method="post">
-          <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Username" name="username">
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password" name="password">
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-          <div class="row">
-            <!-- <div class="col-xs-8">
-              <div class="checkbox icheck">
-                <label>
-                  <input type="checkbox"> Remember Me
-                </label>
-              </div>
-            </div> -->
-            <div class="col-xs-offset-8 col-xs-4">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-            </div>
-          </div>
-        </form>
+            <?php echo show_err_msg($this->session->flashdata('error_msg')); ?>
 
-        <!-- <div class="social-auth-links text-center">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-            Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-            Google+</a>
-        </div> -->
-        <!-- /.social-auth-links -->
-
-        <!-- <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a> -->
-
-      </div>
-      <!-- /.login-box-body -->
-      <?php
-        echo show_err_msg($this->session->flashdata('error_msg'));
-      ?>
+            <form action="<?php echo base_url('Auth/login'); ?>" method="post">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Username" name="username">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" placeholder="Password" name="password">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- /.login-box-body -->
     </div>
-    
 
     <!-- /.login-box -->
 
@@ -87,16 +88,5 @@
     <script src="<?php echo base_url(); ?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
     <!-- Bootstrap 3.3.6 -->
     <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-    <!-- iCheck -->
-    <!-- <script src="<?php echo base_url(); ?>assets/plugins/iCheck/icheck.min.js"></script> -->
-    <!-- <script>
-      $(function () {
-        $('input').iCheck({
-          checkboxClass: 'icheckbox_square-blue',
-          radioClass: 'iradio_square-blue',
-          increaseArea: '20%' // optional
-        });
-      });
-    </script> -->
-  </body>
+</body>
 </html>
